37ad6eac5f2b9fd91953317f0fb88d6d1b54f5f3
Tanner Thelin <tannerthelin@gmail.com>
2017-08-31 11:14:34 -0600

    Added some printed text to Guesser

---
+++ Guesser/Assets/Guessing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guessing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Number Guesser");
+		print ("Pick a number in your head.");
+
+		print ("The highest number you can pick is 100.");
+		print ("The highest number you can pick is 1.");
+
+		print ("Is the number higher or lower than 50?");
+		print ("Up arrow for higher, down for lower, and enter for equal.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
