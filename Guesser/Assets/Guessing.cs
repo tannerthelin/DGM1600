45ffca148b9c55a29dd0c4d2798eb08a046401ac
Tanner Thelin <tannerthelin@gmail.com>
2017-09-05 11:15:49 -0600

    Wrote the code for Guesser Game

--- Guesser/Assets/Guessing.cs
+++ Guesser/Assets/Guessing.cs
@@ -3,21 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Guessing : MonoBehaviour {
+	int max = 100;
+	int min = 1;
+	int guess = 50;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		print ("Welcome to Number Guesser");
 		print ("Pick a number in your head.");
 
-		print ("The highest number you can pick is 100.");
-		print ("The highest number you can pick is 1.");
+		print ("The highest number you can pick is " + max);
+		print ("The highest number you can pick is " + min);
 
-		print ("Is the number higher or lower than 50?");
+		print ("Is the number higher or lower than " + guess);
 		print ("Up arrow for higher, down for lower, and enter for equal.");
+		max = max + 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.UpArrow)) 
+		{
+			min = guess;
+			guess = (max + min) / 2;
+			print ("Is the number higher or lower than " + guess);
+		}
+
+		if (Input.GetKeyDown(KeyCode.DownArrow)) 
+		{
+			max = guess;
+			guess = (max + min) / 2;
+			print ("Is the number higher or lower than " + guess);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Return)) 
+		{
+			print ("Yay, I guessed your number.");
+		}
 	}
 }
