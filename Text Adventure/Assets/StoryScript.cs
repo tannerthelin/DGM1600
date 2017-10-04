1221c6d3d6f3c9aec211dd995f4174318fde5fb2
Tanner Thelin <tannerthelin@gmail.com>
2017-10-04 14:05:06 -0600

    Created Block Breaker game, also added Variables assignment

---
+++ Text Adventure/Assets/StoryScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryScript : MonoBehaviour {
+
+	public Text textObject;
+
+	public enum States {start, forest, cave, shore_0};
+	public States myState;
+
+
+	// Use this for initialization
+	void Start () {
+		myState = States.start;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (myState == States.start) {State_start ();} 
+		else if (myState == States.forest) {State_forest ();}
+		else if (myState == States.cave) {State_cave ();}
+	}
+
+
+	void State_start () {
+		textObject.text = "You are on a small desert island." +
+		"\n There is a FOREST with trees. " +
+		"\n There is also a CAVE." +
+		"\n\n Press F to go to FOREST. Press C to go to CAVE.";
+
+		if (Input.GetKeyDown (KeyCode.F)) {myState = States.forest;}
+		else if (Input.GetKeyDown (KeyCode.C)) {myState = States.cave;}
+	}
+
+	void State_forest () {
+		textObject.text = "Forest State";
+
+		if (Input.GetKeyDown (KeyCode.C)) {myState = States.cave;}
+	}
+
+	void State_cave () {
+		textObject.text = "Cave State";
+
+		if (Input.GetKeyDown (KeyCode.F)) {myState = States.forest;}
+	}
+
+}
